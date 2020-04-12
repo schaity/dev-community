4d7c73c3e54214b72e17dbfc0cddba689a10d9a7
Chaity Swarnaker <schaity.03@gmail.com>
2020-04-12 23:25:27 +0600

    Add user model and registration

---
+++ routes/api/profile.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+
+//@route  Get api/profile
+//@desc   Test route
+//@access public
+router.get('/', (req, res) => res.send('Profile route'));
+module.exports = router;
